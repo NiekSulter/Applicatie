d9a4df24ed0ad41f92ea890cf22ce737077c00b1
NiekSulter <nieksulter99@gmail.com>
2021-05-19 12:39:50 +0200

    Basis

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ from Processing import pubmed_request as pr
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "DEVKEYCHANGEPLEASE"
 
-
 @app.route('/', methods=['POST', 'GET'])
-def hello_world():
+def index():
+    return render_template("home.html")
+
+
+@app.route('/search', methods=['POST', 'GET'])
+def search():
 
     if request.method == 'POST':
         TOR = request.form.getlist('and')
@@ -16,7 +20,12 @@ def hello_world():
 
         pr.make_request(TOR, TAND, str(date))
 
-    return render_template("index.html")
+    return render_template("search.html")
+
+
+@app.route('/results', methods=['POST', 'GET'])
+def vis_results():
+    return render_template("results.html")
 
 
 if __name__ == '__main__':
