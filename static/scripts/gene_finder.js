db3232c50d158584287573afa1a5dce27abe48b8
NiekSulter <nieksulter99@gmail.com>
2021-05-19 17:06:27 +0200

    Form opgeschoond, highlight functionaliteit toegevoegd aan de results

--- static/scripts/gene_finder.js
+++ static/scripts/gene_finder.js
@@ -1,18 +1,18 @@
 $(document).ready(function (){
     var max_input_fields = 6
     var wrapper = $(".input_wrapper")
-    var add_button = $(".add_button")
+    var add_button = $("#add_button")
 
     var counter = 1
     $(add_button).click(function (e){
         e.preventDefault()
         if(counter < max_input_fields) {
             counter++
-            $(wrapper).append('<div><input type="text" name="and"><button class="remove_button" type="button">-</button></div>')
+            $(wrapper).append('<div><input class="form-control input-block mt-2" type="text" name="and"><button class="btn btn-danger mt-2 mb-2 float-right" id="remove_button" type="button">-</button></div>')
         }
     })
 
-    $(wrapper).on("click", ".remove_button", function (e){
+    $(wrapper).on("click", "#remove_button", function (e){
         e.preventDefault()
         $(this).parent('div').remove()
         counter--
@@ -23,18 +23,18 @@ $(document).ready(function (){
 $(document).ready(function (){
     var max_input_fields = 6
     var wrapper = $(".input_wrapper_OR")
-    var add_button = $(".add_button_OR")
+    var add_button = $("#add_button_OR")
 
     var counter = 1
     $(add_button).click(function (e){
         e.preventDefault()
         if(counter < max_input_fields) {
             counter++
-            $(wrapper).append('<div><input type="text" name="or"><button class="remove_button" type="button">-</button></div>')
+            $(wrapper).append('<div><input class="form-control input-block mt-2" type="text" name="or"><button class="btn btn-danger mt-2 mb-2 float-right" id="remove_button" type="button">-</button></div>')
         }
     })
 
-    $(wrapper).on("click", ".remove_button", function (e){
+    $(wrapper).on("click", "#remove_button", function (e){
         e.preventDefault()
         $(this).parent('div').remove()
         counter--
