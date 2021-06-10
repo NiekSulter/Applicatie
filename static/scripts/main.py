f2bbb870029031be1a6046db3fa0d5d26375f2f6
karsten <blok.karsten@gmail.com>
2021-06-10 15:45:58 +0200

    home update

--- static/scripts/main.py
+++ static/scripts/main.py
@@ -6,11 +6,6 @@ from Database.databasemanager import DatabaseManager
 def read_file(bestand):
     df = pandas.read_excel(bestand, dtype=str)
     df = df[df.columns[df.columns.isin(['Symbol_HGNC', 'Aliases', 'GenePanels_Symbol', 'GenePanel', 'GenePanelCount'])]]
-    Symbols = df["Symbol_HGNC"].values
-    Aliases = df["Aliases"].values
-    GenePanel_symbols = df["GenePanels_Symbol"].values
-    GenePanel = df["GenePanel"].values
-    GenePanelCount = df["GenePanelCount"].values
     return df
 
 
@@ -31,12 +26,6 @@ def make_dict(df):
 
         symbol = row['Symbol_HGNC']
         aliases = row['Aliases']
-        #if isinstance(aliases, float):
-        #    aliases_dict[symbol] = []
-
-        #else:
-        #    split_aliases = aliases.split("|")
-        #    aliases_dict[symbol] = split_aliases
         for i in range(len(panels)):
             if isinstance(aliases, float):
                 aliases_dict[symbol] = []
